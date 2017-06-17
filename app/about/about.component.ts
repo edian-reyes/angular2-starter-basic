d01a1060dc384382a6787b1dcdbd5deba9827371
edian-reyes <ereyes@pixnabilab.com>
2017-06-16 22:30:18 -0400

    Initial Commit

---
+++ app/about/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-page',
+  template: `
+    this is the about page
+  `
+})
+
+export class AboutComponent {}
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   styles: [`
-    
+
   `],
   template: `
+    <header>
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a href="#" class="navbar-brand">Angular2 Routing App</a>
+          </div>
+
+          <ul class="nav navbar-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
     <div class="jumbotron text-center">
       <h1>The App Lives!</h1>
       <p>{{ message }}</p>
     </div>
+
+    <home-page></home-page>
+
+    <about-page></about-page>
+
+    <contact-page></contact-page>
   `
 })
 export class AppComponent {
